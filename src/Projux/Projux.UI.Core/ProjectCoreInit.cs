aab368325e5f60d6263f563cf1022e6a3a0d5331
NJKWilson <NJKWilson@outlook.com>
2022-06-25 22:06:09 +0100

    Its working but - Delvelopment Mode everything works - Published mode the _content folder is published in a different location and messes everything up

---
+++ src/Projux/Projux.UI.Core/ProjectCoreInit.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace Projux.UI.Core
+{
+    public static class ProjectCoreInit
+    {
+        public static void AddProjuxCore(this IServiceCollection serviceCollection)
+        {
+            // Services
+            serviceCollection.AddMudServices();
+        }
+    }
+}
--- src/Projux/Projux.UI.WPF/MainWindow.xaml.cs
+++ src/Projux/Projux.UI.WPF/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Projux.UI.Core.Data;
+using Projux.UI.Core;
 
 namespace Projux.UI.WPF
 {
@@ -23,6 +13,12 @@ namespace Projux.UI.WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddProjuxCore();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
         }
     }
 }
