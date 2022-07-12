289e97a7d4bc6345bcfc2e307309dd3c2b95b153
NJKWilson <NJKWilson@outlook.com>
2022-07-12 21:29:40 +0100

    Added test project

---
+++ src/Projux/Projux.Backend.Core.Tests/BasicServices/CustomerContact/CustomerContactServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using Projux.Backend.Core.Tests.Fixtures.Collection.LiteDbContext;
+using Projux.Backend.Core.BasicServices.CustomerContact;
+using LiteDB;
+using Projux.Backend.Core.BasicServices.CustomerContact.Exceptions;
+
+namespace Projux.Backend.Core.Tests.BasicServices.CustomerContact;
+
+[Collection("LiteDbContextCollection")]
+public class CustomerContactServiceTests
+{
+    private readonly LiteDbContextFixture _liteDbContextFixture;
+    private readonly ICustomerContactService _customerContactService;
+
+    public CustomerContactServiceTests(LiteDbContextFixture liteDbContextFixture)
+    {
+        _liteDbContextFixture = liteDbContextFixture;
+        _customerContactService = new CustomerContactService(_liteDbContextFixture.Db);
+    }
+
+    [Fact]
+    public void FindById_UsingRandomId_ShouldThrowException()
+    {
+        var randomId = ObjectId.NewObjectId();
+        Assert.Throws<CustomerContactServiceNotFoundException>(() => _customerContactService.FindById(randomId));
+    }
+
+    [Fact]
+    public void FindById_UsingBlankId_ShouldThrowException()
+    {
+        var blankId = ObjectId.Empty;
+        Assert.Throws<CustomerContactServiceValidationException>(() => _customerContactService.FindById(blankId));
+    }
+
+
+    [Fact]
+    public void Insert_RandomCustomer_ShouldReturnVailidObjectId()
+    {
+        var customerContact = new Database.Entities.CustomerContact
+        {
+            FirstName = ObjectId.NewObjectId().ToString(),
+            LastName = ObjectId.NewObjectId().ToString(),
+            EmailAddress = ObjectId.NewObjectId().ToString(),
+            PhoneNumber = ObjectId.NewObjectId().ToString()
+        };
+
+        var result = _customerContactService.Insert(customerContact);
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void Insert_TwoSameIdo_ShouldDoSomething()
+    {
+        var customerContact = new Database.Entities.CustomerContact
+        {
+            Id = ObjectId.NewObjectId(),
+            FirstName = ObjectId.NewObjectId().ToString(),
+            LastName = ObjectId.NewObjectId().ToString(),
+            EmailAddress = ObjectId.NewObjectId().ToString(),
+            PhoneNumber = ObjectId.NewObjectId().ToString()
+        };
+
+        var result1 = _customerContactService.Insert(customerContact);
+        Assert.Throws<CustomerContactServiceDependencyException>(() => _customerContactService.Insert(customerContact));
+    }
+}
---
+++ src/Projux/Projux.Backend.Core.Tests/Fixtures/Collection/LiteDbContext/LiteDbContextFixture.cs
@@ -0,0 +1,19 @@
+﻿namespace Projux.Backend.Core.Tests.Fixtures.Collection.LiteDbContext;
+
+using Projux.Backend.Core.Database;
+
+public class LiteDbContextFixture : IDisposable
+{
+    public LiteDbContextFixture()
+    {
+        Db = new Database.LiteDbContext(new LiteDbOptions { DatabaseLocation = @"LiteDbTest.db" });
+    }
+
+    public void Dispose()
+    {
+        Db.Database.Dispose();
+        File.Delete(@"LiteDbTest.db");
+    }
+
+    public ILiteDbContext Db { get; private set; }
+}
